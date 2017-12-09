1d7f0724ea5031a6666f7950b40f4d3ecb227e58
jorge  ivan urueta  ramos <jorgeriur96@gmail.com>
2017-12-08 23:39:20 -0500

    iniciar

---
+++ app/src/main/java/com/unamjorge/practicas/mascotapetagram/Correo/Config.java
@@ -0,0 +1,11 @@
+package com.unamjorge.practicas.mascotapetagram.Correo;
+
+/**
+ * Created by Jorge Urueta on 8/12/2017.
+ */
+
+public class Config {
+
+    public static final String EMAIL ="jorgeriur96@gmail.com";
+    public static final String PASSWORD ="conalco96_";
+}
